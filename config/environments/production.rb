70ae208ecf1893f42fa515c77e19320c274e0ec4
Torsten Ruger <torsten@villataika.fi>
2016-03-30 18:33:46 +0300

    no asset compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,9 @@
+class Transformer
+  def compress(string)
+    string
+  end
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -25,8 +31,8 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  #config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = Transformer.new
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
