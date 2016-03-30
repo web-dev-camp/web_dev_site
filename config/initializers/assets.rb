5d99301958256de012220d336c55dd335591790d
Torsten Ruger <torsten@villataika.fi>
2016-03-30 18:31:39 +0300

    use era to fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( scripts.js layerslider_pack.js smoothscroll.js demo.layerslider_slider.js)
 
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
