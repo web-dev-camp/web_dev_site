2f454aed170d01e23498774bf2a7175f1149ad6c
Torsten Ruger <torsten@villataika.fi>
2016-03-31 13:51:27 +0300

    remove sass hack
    which was due to not working image-url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,3 @@
-class Transformer
-  def compress(string)
-    string
-  end
-end
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -28,11 +22,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = Transformer.new
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
