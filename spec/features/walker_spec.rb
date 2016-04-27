60e0cedaaf393c5f2b361b2df73843e15f288027
Torsten Ruger <torsten@villataika.fi>
2016-04-27 16:36:33 +0300

    add missing template

---
+++ spec/features/walker_spec.rb
@@ -0,0 +1,26 @@
+describe "Site integrity"  do
+  before :all do
+    @pages = {}
+  end
+
+  def walks url
+    return if url.include? "http"
+    return if url.include? "mailto"
+    begin
+      visit url
+    rescue
+      puts "Failed #{url}"
+    end
+    page.all("a").each do |p|
+      ref = p[:href]
+      next if @pages.has_key? ref
+      @pages[ref] = false
+      walks ref
+    end
+  end
+
+  it "renders ok" do
+    walks "/"
+  end
+
+end
