644160faf1e805dd48aa83f3078c0563002db24f
Torsten Ruger <torsten@villataika.fi>
2016-05-28 15:39:56 +0300

    fix user name
    was getting lost in strong parameters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,12 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     page_path(:camp)
   end
+  private
+  def sign_up_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+ end
+
+ def account_update_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+ end
 end
