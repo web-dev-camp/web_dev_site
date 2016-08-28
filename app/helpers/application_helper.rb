a2a61b3f539ba162467af42a2013a52c974beb84
Torsten Ruger <torsten@villataika.fi>
2016-08-28 11:19:47 +0300

    move helper to rightful place

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,21 +24,6 @@ module ApplicationHelper
     registration_url(:user , ssl_hash )
   end
 
-  # define a bunch of defaults for the best_in_place call
-  def resume_in_place field , attributes = {}
-    txt = I18n.t("resumes.#{field}")
-    br = proc {|txtt| raw(txtt.to_s.gsub("\n" , "<br>")) }
-    defaults = { :url => resume_path , place_holder: txt , display_with: br}
-    unless [:street , :city , :country].include? field
-      defaults.merge! ok_button: "Edit", ok_button_class: "btn btn-success" ,
-                      cancel_button: "Cancel" , cancel_button_class:  "btn btn-warning",
-                      inner_class:  "form-control" , as: :textarea ,
-                      sanitize: false , html_attrs: {rows: 10}
-    end
-    attributes.reverse_merge! defaults
-    best_in_place(@resume , field , attributes)
-  end
-
   def social_link href , kind , &block
     link_to(href , {class: "social-icon social-icon-sm social-#{kind}" , target: :blank } , &block)
   end
--- app/helpers/resumes_helper.rb
+++ app/helpers/resumes_helper.rb
@@ -1,2 +1,30 @@
 module ResumesHelper
+
+
+
+
+  def percent
+    fields = [:school, :uni, :internship, :work, :projects, :tech_skills, :soft_skills,
+              :interests, :motivation, :finance, :other, :street, :city, :country]
+    total = fields.length
+    filled = fields.collect{|f| @resume.send(f).blank? ? 0 : 1 }.sum * 100
+    "#{filled/total}%"
+  end
+
+  # define a bunch of defaults for the best_in_place call
+  def resume_in_place field , attributes = {}
+    txt = I18n.t("resumes.#{field}")
+    button = @resume.send(field).blank? ? "Ok" : "Edit"
+    br = proc {|txtt| raw(txtt.to_s.gsub("\n" , "<br>")) }
+    defaults = { :url => resume_path , place_holder: txt , display_with: br}
+    unless [:street , :city , :country].include? field
+      defaults.merge! ok_button: button,  ok_button_class: "btn btn-success" ,
+                      cancel_button: "Cancel" , cancel_button_class:  "btn btn-warning",
+                      inner_class:  "form-control" , as: :textarea ,
+                      sanitize: false , html_attrs: {rows: 10}
+    end
+    attributes.reverse_merge! defaults
+    best_in_place(@resume , field , attributes)
+  end
+
 end
