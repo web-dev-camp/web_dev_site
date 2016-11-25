f72c7de618f6150351d1b7d360367baf7d07fd6a
Torsten Ruger <torsten@villataika.fi>
2016-11-26 00:14:43 +0200

    fix test

--- spec/features/visitors/course_page_spec.rb
+++ spec/features/visitors/course_page_spec.rb
@@ -2,8 +2,8 @@ feature 'About page' do
 
   scenario 'Visit the course page' do
     visit 'course'
-    expect(page).to have_content '8 week'
-    expect(page).to have_content 'live in'
+    expect(page).to have_content 'Web Application Development'
+    expect(page).to have_content 'Technical skill is the start'
     expect(page).to have_content 'You will give 2'
   end
 
