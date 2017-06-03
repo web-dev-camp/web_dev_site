e1f62d66687bf5f91d2726707bc2d8a9e9426862
Torsten Ruger <torsten@villataika.fi>
2017-06-03 22:17:13 +0300

    change migration to move old data over

--- db/migrate/20170602115219_add_street_city_country_to_user.rb
+++ db/migrate/20170602115219_add_street_city_country_to_user.rb
@@ -1,7 +1,24 @@
 class AddStreetCityCountryToUser < ActiveRecord::Migration[5.0]
-  def change
+
+  def up
     add_column :users, :street, :string
     add_column :users, :city, :string
     add_column :users, :country, :string
+    puts "UP"
+    User.all.each do |user|
+      resume = user.resume
+      user.street = resume.street
+      user.city = resume.city
+      user.country = resume.country
+      user.save
+    end
   end
+
+  def down
+    remove_column :users, :street
+    remove_column :users, :city
+    remove_column :users, :country
+    puts "DOWN"
+  end
+
 end
