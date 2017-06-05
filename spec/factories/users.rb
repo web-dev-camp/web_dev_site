cdd87bf56b385353a973b79ccee775e0f9c418bc
Torsten Ruger <torsten@villataika.fi>
2017-06-05 16:26:15 +0300

    fix user factory
    and test accordingly

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,16 +2,16 @@ FactoryGirl.define do
   factory :user do
     confirmed_at Time.now
     name "Test User"
-    email "test@example.com"
+    sequence( :email) { |n| "user#{n}@test.com" }
     password "please123"
 
     street "WhereIlive 111"
     city "CityOfTesters"
     country "WhereIam"
-    
-    trait :admin do
-      role 'admin'
-    end
 
+    factory :admin do
+      sequence( :email) { |n| "admin#{n}@test.com" }
+      role :admin
+    end
   end
 end
--- spec/mailers/apply_spec.rb
+++ spec/mailers/apply_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ApplyMailer, type: :mailer do
 
     it "renders the headers" do
       expect(mail.subject).to match("submitted")
-      expect(mail.to).to eq(["test@example.com"])
+      expect(mail.to.first).to match(/user.*test.com/)
       expect(mail.from).to eq(["admission@webdev.camp"])
     end
 
@@ -20,7 +20,7 @@ RSpec.describe ApplyMailer, type: :mailer do
 
     it "renders the headers" do
       expect(mail.subject).to match("cancelled")
-      expect(mail.to).to eq(["test@example.com"])
+      expect(mail.to.first).to match(/user.*test.com/)
       expect(mail.bcc).to eq(["admission@webdev.camp"])
       expect(mail.from).to eq(["admission@webdev.camp"])
     end
