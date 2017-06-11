ae15b296ea52019ea877d7a9749f2c9da5a740b3
Torsten Ruger <torsten@villataika.fi>
2017-06-11 23:02:11 +0300

    test for the json call

--- spec/features/resume/resume_edit_spec.rb
+++ spec/features/resume/resume_edit_spec.rb
@@ -12,6 +12,14 @@ feature 'Resume edit' do
     expect(page).to have_content("Resume")
   end
 
+  scenario 'progress json call works' do
+    user = sign_new
+    visit resume_path(format: :json)
+    user.resume.percent_attributes.each do |name|
+      expect(page).to have_content(name)
+    end
+  end
+
   scenario "progress works" do
     sign_new
     visit_url progress_resume_url()
