0c69d404d1d65a9a37760afe613aec80727b3d23
Torsten Ruger <torsten@villataika.fi>
2017-06-14 19:37:05 +0300

    fix tests

--- spec/features/blog/page_spec.rb
+++ spec/features/blog/page_spec.rb
@@ -2,12 +2,11 @@
 feature 'Page show' do
 
   scenario 'page shows for correct title' do
-    visit_url blog_page_url("Basic-instructions-for-writing-blogs")
-    expect(page).to have_content("instructions")
+    visit_url blog_page_url("the-distraction-free-path-to-becoming-a-professional-programmer")
   end
   scenario 'page title shows for correct title' do
-    visit_url blog_page_url("Basic-instructions-for-writing-blogs")
-    expect(page).to have_content("Headings")
+    visit_url blog_page_url("the-distraction-free-path-to-becoming-a-professional-programmer")
+    expect(page).to have_content("Professional Programmer")
   end
 
 end
