221a2439b92e56c96a0df62448c5de583f2b0adf
Torsten Ruger <torsten@villataika.fi>
2017-06-15 21:01:25 +0300

    rename blog images and make helper

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,8 +1,11 @@
 module BlogHelper
-  def page_content(page)
-    Kramdown::Document.new(page.content).to_html.html_safe
-  end
 
+  def blog_image(name)
+    image_tag "blog/#{name}" , class: :large
+  end
+  def blog_header(name)
+    image_tag "blog/#{name}"
+  end
   def page_summary(page)
     content = page_content(page)
     content[0 ... 200]
